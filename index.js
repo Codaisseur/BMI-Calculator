61c270a6718776157937e4beda36f5df8e04f271
Reinoptland <reinoptland@gmail.com>
2020-02-09 19:56:53 +0100

    Calculate BMI and assign to variable

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var heightInM = 1.79;
 
 console.log(weightInKg);
 console.log(heightInM);
+
+// The formula for BMI is: weight (kg) / (height (m) x height (m))
+var BMI = weightInKg / (heightInM * heightInM);
+
+console.log(BMI);
