6e225686f3ce731ac5838ea29e0f95e092827c8c
Reinoptland <reinoptland@gmail.com>
2020-02-09 19:59:45 +0100

    Format output using a template string

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var weightInKg = 82;
 var heightInM = 1.79;
 
-console.log(weightInKg);
-console.log(heightInM);
-
 // The formula for BMI is: weight (kg) / (height (m) x height (m))
 var BMI = weightInKg / (heightInM * heightInM);
 
-console.log(BMI);
+console.log(`
+**************
+BMI CALCULATOR
+**************
+
+height: ${heightInM}
+weight: ${weightInKg}
+
+****************
+FACING THE FACTS
+****************
+
+Your BMI is ${BMI}
+
+A BMI under 18.5 is considered underweight
+A BMI above 25 is considered overweight
+`);
