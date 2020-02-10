4a4f479f6eaceba6e663d1e8d7aac75709ae8047
Reinoptland <reinoptland@gmail.com>
2020-02-10 12:37:34 +0100

    Hardcode data for dailyExercise

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var weightInKg = parseInt(process.argv[2]);
 var heightInM = parseFloat(process.argv[3]);
 var age = parseInt(process.argv[4]);
+var dailyExercise = "yes";
+
+// Checking myself
+console.log(dailyExercise);
 
 // The formula for BMI is: weight (kg) / (height (m) x height (m))
 var BMI = weightInKg / (heightInM * heightInM);
